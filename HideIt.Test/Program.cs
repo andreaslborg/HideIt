6f859bd1e1ddb881d63e3a76fc606940bf012347
Andreas Lykke Borg <72602768+andreaslborg@users.noreply.github.com>
2026-08-11 20:13:45 +0200

    Added HideIt test site (Closes #21) (#24)
    * Added HideIt test site
    
    * Updated content using hideIt

---
+++ HideIt.Test/Program.cs
@@ -0,0 +1,30 @@
+using HideIt.Test.Middleware;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+// Add security headers middleware
+app.UseMiddleware<SecurityHeadersMiddleware>();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
