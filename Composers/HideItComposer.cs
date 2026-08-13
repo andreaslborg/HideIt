4f8ba37ab828f5f7218b219ea95ae196812c2a3d
Andreas Lykke Borg <72602768+andreaslborg@users.noreply.github.com>
2026-08-13 13:47:21 +0200

    Added IHideItBlockListFilter

--- Composers/HideItComposer.cs
+++ Composers/HideItComposer.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Hide It Package.
 // MIT Licensed.
 
+using HideIt.Filters;
 using HideIt.ValueConverters;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +14,7 @@ namespace HideIt.Composers;
 
 /// <summary>
 /// Composer that registers Hide It services.
-/// Replaces the built-in block property value converters to automatically filter hidden blocks.
+/// Replaces the built-in block property value converters and registers the Hide It filter pipeline.
 /// </summary>
 [UsedImplicitly]
 public class HideItComposer : IComposer {
@@ -24,6 +25,8 @@ public class HideItComposer : IComposer {
       HideItSettings settings = new( configuredAlias );
 
       builder.Services.AddSingleton( Options.Create( settings ) );
+      builder.Services.AddSingleton<IHideItBlockListFilter, HideItVisibilityBlockListFilter>();
+      builder.Services.AddSingleton<IHideItBlockGridFilter, HideItVisibilityBlockGridFilter>();
       BlockExtensions.SetPropertyAlias( settings.PropertyAlias );
 
       // Replace the built-in converters with our HideIt-aware versions
