92a63f408d6400b7c09bb71f1b25a642d3db86d0
Andreas Lykke Borg <72602768+andreaslborg@users.noreply.github.com>
2026-08-13 13:56:50 +0200

    Added IHideItBlockListFilter (#30)

--- ValueConverters/HideItBlockListPropertyValueConverter.cs
+++ ValueConverters/HideItBlockListPropertyValueConverter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Hide It Package.
 // MIT Licensed.
 
+using HideIt.Filters;
 using JetBrains.Annotations;
 using Umbraco.Cms.Core.DeliveryApi;
 using Umbraco.Cms.Core.Logging;
@@ -18,6 +19,8 @@ namespace HideIt.ValueConverters;
 /// </summary>
 [UsedImplicitly]
 public class HideItBlockListPropertyValueConverter : BlockListPropertyValueConverter {
+  private readonly IReadOnlyList<IHideItBlockListFilter> _filters;
+
   /// <inheritdoc />
   public HideItBlockListPropertyValueConverter(
       IProfilingLogger proflog,
@@ -29,8 +32,10 @@ public class HideItBlockListPropertyValueConverter : BlockListPropertyValueConve
       IVariationContextAccessor variationContextAccessor,
       BlockEditorVarianceHandler blockEditorVarianceHandler,
       ILanguageService languageService,
-      IPropertyRenderingContextAccessor propertyRenderingContextAccessor )
+      IPropertyRenderingContextAccessor propertyRenderingContextAccessor,
+      IEnumerable<IHideItBlockListFilter> filters )
       : base( proflog, blockConverter, contentTypeService, apiElementBuilder, jsonSerializer, constructorCache, variationContextAccessor, blockEditorVarianceHandler, languageService, propertyRenderingContextAccessor ) {
+    _filters = filters.ToArray();
   }
 
   /// <inheritdoc />
@@ -38,11 +43,19 @@ public class HideItBlockListPropertyValueConverter : BlockListPropertyValueConve
     object? result = base.ConvertIntermediateToObject( owner, propertyType, referenceCacheLevel, inter, preview );
 
     return result switch {
-      // Filter hidden blocks from BlockListModel
-      BlockListModel blockList => blockList.WhereVisible(),
+      // Filter hidden blocks from BlockListModel and apply registered extensions
+      BlockListModel blockList => ApplyFilters( blockList ),
       // Filter hidden single block items
       BlockListItem blockItem when blockItem.IsBlockHidden() => null,
       _ => result
     };
+  }
+
+  private BlockListModel ApplyFilters( BlockListModel model ) {
+    BlockListModel currentModel = model;
+    foreach ( IHideItBlockListFilter filter in _filters ) {
+      currentModel = filter.Filter( currentModel ) ?? throw new InvalidOperationException( $"HideIt block list filter '{filter.GetType().FullName}' returned null." );
+    }
+    return currentModel;
   }
 }
